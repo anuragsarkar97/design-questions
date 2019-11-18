991c9833ea38aafc09a6de570980ba572ba53077
Anurag Sarkar <anurag.sarkar@oyorooms.com>
2019-11-18 22:14:13 +0530

    updates

--- src/main/java/io/gojek/parkinglot/service/impl/ParkingInitializeService.java
+++ src/main/java/io/gojek/parkinglot/service/impl/ParkingInitializeService.java
@@ -4,14 +4,14 @@ import io.gojek.parkinglot.models.ParkingLot;
 import io.gojek.parkinglot.service.ParkingLotService;
 
 import static io.gojek.parkinglot.costants.ParkingLotConstants.CREATED_PARKING_LOT;
+import static io.gojek.parkinglot.costants.ParkingLotConstants.INVALID_COMMAND;
 
 public class ParkingInitializeService implements ParkingLotService<Void, String> {
 
 
     public Void executeInstruction(ParkingLot parkingLot, String... argument) {
         if (argument.length != 2) {
-            //TODO :: Invalid instruction and throw error
-            //        Try moving this to validation
+            System.out.println(INVALID_COMMAND);
         }
         Integer parkingLotSize = Integer.valueOf(argument[1]);
         ParkingLot newParkingLot = new ParkingLot(parkingLotSize);
