3ece86ff600925fe27ef6ac8419ae4e8628152d0
Dilip Devaram <dilipdevaram@Dilips-MacBook-Pro.local>
2020-04-25 22:00:59 +0800

    adding game loop

--- battleship/src/main/java/io/swiggy/battleship/game/GameLoop.java
+++ battleship/src/main/java/io/swiggy/battleship/game/GameLoop.java
@@ -1,13 +1,39 @@
 package io.swiggy.battleship.game;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class GameLoop {
 
-    public void startGameLoop() {
-        // start new game, resume existing one
+    public void startGameLoop() throws IOException {
+        while (true) {
+            System.out.println("Please select from optino below \n" +
+                    "1. Start a new game \n" +
+                    "2. Resume one of the previous games");
 
-        // listen for two options
-        //  1. start new game
-        //  2. Resume previous games
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            int option;
+            try {
+                 option = Integer.parseInt(reader.readLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input");
+                continue;
+            }
+
+            switch (option) {
+                case 1:
+                    System.out.println("Staring a new games...");
+                    break;
+                case 2:
+                    System.out.println("Staring existing game...");
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+
+        }
 
     }
 
